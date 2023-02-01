a43b66b2ec49418ed331dbf476f5d67a6f820a5b
Gintare Simutyte <gintare.simutyte@gmail.com>
2023-02-01 23:52:27 +0100

    node v 14

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,14 @@ export default function Home({ posts }) {
       </Head>
 
       <main className="flex flex-col items-center flex-1 px-20 py-10">
-        <h1 className="text-6xl font-bold mt-5 mb-5">Blog</h1>
-        <p className="text-xl  mb-5">WordPress as a Headless CMS with React</p>
+        <h1 className="mt-5 mb-5 text-6xl font-bold">Blog</h1>
+        <p className="mb-5 text-xl">WordPress as a Headless CMS with React</p>
         {posts && (
           <div className="grid grid-cols-2 gap-5">
             {posts.map((post, id) => {
               return (
                 <div key={id}>
+                  <div>my post</div>
                   <Post post={post} />
                 </div>
               );
