dab26240aacbf477187a686618fcccfb061f3f59
Gintare Simutyte <gintare.simutyte@gmail.com>
2023-04-17 17:36:15 +0200

    added express

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/deploy', (req, res) => {
-  // Handle the POST request here
-  // Pull and run the Docker image from the GitHub Registry Packages
-  // ...
-  res.status(200).json({ success: true, message: 'Yeah deployment successful from get post' });
-});
-
 app.post('/deploy', (req, res) => {
   // Handle the POST request here
   // Pull and run the Docker image from the GitHub Registry Packages
