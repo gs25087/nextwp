93647b0c45f31554d343f4b59b790c347a454a68
Gintare Simutyte <gintare.simutyte@gmail.com>
2023-04-17 16:50:20 +0200

    added express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/deploy', (req, res) => {
+  // Handle the POST request here
+  // Pull and run the Docker image from the GitHub Registry Packages
+  // ...
+  res.status(200).json({ success: true, message: 'Yeah deployment successful' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
